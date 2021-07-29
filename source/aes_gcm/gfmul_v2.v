fd32c403538c7888c59439a42d466d42dbf8cf8a
Duong Tuan-Kiet <tuankiet@pc94.VLSILAB>
2021-07-29 14:37:28 +0900

    working gfmul v2

--- source/aes_gcm/gfmul_v2.v
+++ source/aes_gcm/gfmul_v2.v
@@ -1,7 +1,7 @@
 module gfmul_v2(
 input iClk,
 input iRst_n,
-input [127:0] iCtext,
+input [0:127] iCtext,
 input iCtext_valid,
 input [0:127] iHashkey,
 input iHashkey_valid,
@@ -18,15 +18,20 @@ assign iR = {8'b1110_0001, 120'd0};
 wire [0:127] V_and_xor;
 wire [0:127] Z_and_xor;
 reg [6:0] cnt;
-wire [0:127] mux_Z;
+wire [0:127] mux_Z_1;
+wire [0:127] mux_Z_2;
 wire [0:127] mux_V;
-wire mux_sel;
+wire mux_sel_V;
+wire mux_sel_Z_1;
+wire mux_sel_Z_2;
 
 //----------------------------------------------------------------
 // function
 //----------------------------------------------------------------
-function and_xor;
-	input [0:127] in1, in2, in3;
+function [0:127]and_xor;
+	input [0:127] in1;
+	input [0:127] in2;
+	input [0:127] in3;
 	begin
 		and_xor = in1 ^ (in2 & in3);
 	end
@@ -36,7 +41,7 @@ endfunction
 // register
 //----------------------------------------------------------------
 always@(posedge iClk) begin
-	if(~iRst_n | oResult_valid)								cnt <= 7'd0;
+	if(~iRst_n | oResult_valid)				cnt <= 7'd0;
 	else if(iCtext_valid && iHashkey_valid) cnt <= cnt + 1'b1;
 	else									cnt <= cnt;
 end
@@ -47,7 +52,7 @@ always@(posedge iClk) begin
 end
 
 always@(posedge iClk) begin
-	if(iCtext_valid) 	Z <= Z_and_xor;
+	if(iCtext_valid && iHashkey_valid) 	Z <= Z_and_xor;
 	else 				Z <= Z;
 end
 
@@ -56,13 +61,16 @@ end
 //----------------------------------------------------------------
 assign oResult_valid = &cnt;
 assign oResult = Z;
-// assign mux_sel = ~^cnt;
-assign mux_sel = (cnt == 7'd0)? 1'b1 : 1'b0;
-assign mux_V = (mux_sel)? iHashkey : {1'b0, V[0:126]} ;
-assign V_and_xor = and_xor(mux_V, iR, {128{V[127]}});
-assign mux_Z = (mux_sel)? 128'd0 : Z;
-assign Z_and_xor = and_xor(Z, V_and_xor, {128{iCtext[cnt]}});
 
+assign mux_sel_V = (cnt == 7'd0)? 1'b1 : 1'b0;
+assign mux_sel_Z_1 = (cnt == 7'd0)? 1'b1 : 1'b0;
+assign mux_sel_Z_2 = (cnt == 7'd0)? 1'b1 : 1'b0;
+
+assign mux_V = (mux_sel_V)? iHashkey : V;
+assign V_and_xor = and_xor({1'b0, mux_V[0:126]}, iR, {128{mux_V[127]}});
+
+assign mux_Z_1 = (mux_sel_Z_1)? iHashkey : V;
+assign mux_Z_2 = (mux_sel_Z_2)? 128'd0 : Z;
+assign Z_and_xor = and_xor(mux_Z_2, mux_Z_1, {128{iCtext[cnt]}});
 
 endmodule
-
