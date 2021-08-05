bde371ea5e3717da98bd7a4abe38b5d15f28b55a
Duong Tuan-Kiet <tuankiet@pc94.VLSILAB>
2021-08-05 17:17:45 +0900

    add iNext

--- source/aes_gcm/ghash_block_v2.v
+++ source/aes_gcm/ghash_block_v2.v
@@ -1,6 +1,8 @@
 module ghash_block_v2 (
 input 	iClk,
 input 	iRstn,
+	//control
+input iNext,
 	//data
 input 	[0:127] iCtext,
 input 	iCtext_valid,
@@ -17,6 +19,8 @@ wire [0:127] wXor;
 gfmul_v2 GFMUL(
 .iClk(iClk),
 .iRstn(iRstn),
+	//control
+.iNext(iNext),
 	//data
 .iCtext(wXor),
 .iCtext_valid(iCtext_valid),
